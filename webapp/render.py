4e3b56e126d7af1034741c5e4ca83ec2e4582a44
Daniel Finch <dlukefinch@daniels-mac-mini.local>
2026-09-09 21:28:07 +0100

    Update of title

--- webapp/render.py
+++ webapp/render.py
@@ -57,7 +57,7 @@ def build_demo() -> dict:
 
 
 _PAGE = r"""<!doctype html>
-<title>SLM &mdash; UK Snow Outlook</title>
+<title>Snow Watch SLM</title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=IBM+Plex+Mono:wght@400;500;600&display=swap" rel="stylesheet">
